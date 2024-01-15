d0937a0e30fbdc890a36b3ef9e3f03118f36fcd1
Mostafa <candlelearner.m.dev@gmail.com>
2024-01-15 21:03:48 +0100

    heello

--- src/App.js
+++ src/App.js
@@ -51,26 +51,28 @@ class App extends Component {
       input: " ",
       imageURL: "",
       box: {},
+      route: "signin",
     };
   }
 
   calcPosition = (data) => {
-    const positionsBox = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const positionsBox =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const height = Number(image.height);
     const width = Number(image.width);
 
-    return ({
+    return {
       left: positionsBox.left_col * width,
       top: positionsBox.top_row * height,
-      right: width - (positionsBox.right_col * width),
-      bottom: height - (positionsBox.bottom_row * height),
-    })
-  }
+      right: width - positionsBox.right_col * width,
+      bottom: height - positionsBox.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
-    this.setState({box: box})
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -82,41 +84,41 @@ class App extends Component {
       "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
       returnClarifaiRequestOption(this.state.input)
     )
-    .then((response) => response.json())
-    .then((result) => this.displayFaceBox(this.calcPosition(result)))
-    .catch((error) => console.log("error", error));
+      .then((response) => response.json())
+      .then((result) => this.displayFaceBox(this.calcPosition(result)))
+      .catch((error) => console.log("error", error));
     // .then((result) => {
-      //   const regions = result.outputs[0].data.regions;
-      //   console.log(regions);
+    //   const regions = result.outputs[0].data.regions;
+    //   console.log(regions);
 
-      //   regions.forEach((region) => {
-      //     // Accessing and rounding the bounding box values
-      //     const boundingBox = region.region_info.bounding_box;
-      //     const topRow = boundingBox.top_row.toFixed(3);
-      //     const leftCol = boundingBox.left_col.toFixed(3);
-      //     const bottomRow = boundingBox.bottom_row.toFixed(3);
-      //     const rightCol = boundingBox.right_col.toFixed(3);
+    //   regions.forEach((region) => {
+    //     // Accessing and rounding the bounding box values
+    //     const boundingBox = region.region_info.bounding_box;
+    //     const topRow = boundingBox.top_row.toFixed(3);
+    //     const leftCol = boundingBox.left_col.toFixed(3);
+    //     const bottomRow = boundingBox.bottom_row.toFixed(3);
+    //     const rightCol = boundingBox.right_col.toFixed(3);
 
-      //     region.data.concepts.forEach((concept) => {
-      //       // Accessing and rounding the concept value
-      //       const name = concept.name;
-      //       const value = concept.value.toFixed(4);
+    //     region.data.concepts.forEach((concept) => {
+    //       // Accessing and rounding the concept value
+    //       const name = concept.name;
+    //       const value = concept.value.toFixed(4);
 
-      //       this.setState({
-      //         box: {
-      //           top: topRow * 100,
-      //           bottom: bottomRow * 100,
-      //           left: leftCol * 100,
-      //           right: rightCol * 100,
-      //         },
-      //       });
+    //       this.setState({
+    //         box: {
+    //           top: topRow * 100,
+    //           bottom: bottomRow * 100,
+    //           left: leftCol * 100,
+    //           right: rightCol * 100,
+    //         },
+    //       });
 
-      //       console.log(
-      //         `${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`
-      //       );
-      //     });
-      //   });
-      // })
+    //       console.log(
+    //         `${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`
+    //       );
+    //     });
+    //   });
+    // })
   };
 
   render() {
@@ -124,15 +126,23 @@ class App extends Component {
       <div className="App white w-100">
         <div className=" bg-image"></div>
         <BackgroundEffect className="w-100" />
-        <Login />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForum
-          onInputChange={this.onInputChange}
-          onButtonClick={this.onButtonClick}
-        />
-        <FaceRecognition imageURL={this.state.imageURL} box={this.state.box} />
+        {this.state.route === "signin" ? (
+          <Login />
+        ) : (
+          <>
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForum
+              onInputChange={this.onInputChange}
+              onButtonClick={this.onButtonClick}
+            />
+            <FaceRecognition
+              imageURL={this.state.imageURL}
+              box={this.state.box}
+            />
+          </>
+        )}
       </div>
     );
   }
