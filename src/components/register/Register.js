854d966e92236e9571c45a4266f633ccc48a48b3
Mostafa <candlelearner.m.dev@gmail.com>
2024-01-16 20:35:45 +0100

    hello

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -46,7 +46,7 @@ function Register({ onRouteChange }) {
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Sign in"
-              onClick={() => onRouteChange("home")}
+              onClick={() => onRouteChange("register")}
             />
           </div>
         </form>
